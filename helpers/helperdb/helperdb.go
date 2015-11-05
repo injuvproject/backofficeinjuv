195423476013a80ac45275edb024a7c018ce4725
Ignacio Carvajal D <ignaciocarvajald@gmail.com>
2015-11-05 18:21:54 -0300

    se modifico la cadena de conexion

--- helpers/helperdb/helperdb.go
+++ helpers/helperdb/helperdb.go
@@ -3,5 +3,5 @@ package helperdb
 import "github.com/jmoiron/sqlx"
 
 func GetDatabase() *sqlx.DB {
-	return sqlx.MustConnect("mysql", "root:root@tcp(localhost:3306)/go-vipex?charset=utf8mb4,utf8&parseTime=true")
+	return sqlx.MustConnect("mysql", "root:volcom..@tcp(localhost:3306)/DB_INJUV_Backoffice?charset=utf8mb4,utf8&parseTime=true")
 }
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os/user"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/injuvproject/backofficeinjuv/handlers"
 	"github.com/injuvproject/backofficeinjuv/helpers/helperdb"
 	"github.com/injuvproject/backofficeinjuv/middleware/database"
 	"github.com/injuvproject/backofficeinjuv/middleware/renderer"
+	"github.com/jmoiron/sqlx"
 	"github.com/pressly/cji"
 	"github.com/theosomefactory/goji-static"
+	"github.com/unrolled/render"
 	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
 )
 
 func main() {
@@ -22,6 +28,23 @@ func main() {
 	goji.Get("/", http.RedirectHandler("/login.html", http.StatusFound))
 	goji.Get("/login.html", handlers.GetLogin)
 	goji.Post("/login.html", cji.Use(database.InjectDatabase(db)).On(handlers.PostLogin))
+	goji.Get("/test", cji.Use(database.InjectDatabase(db)).On(func(c web.C, w http.ResponseWriter, r *http.Request) {
+		render := c.Env["render"].(*render.Render)
+		db := c.Env["mysql"].(*sqlx.DB)
+		newuser := user.User{}
 
+		err := db.Get(&newuser, "SELECT * FROM Usuario WHERE id=1", "newuser")
+		if err != nil {
+			fmt.Printf("error %#v\n")
+		}
+		fmt.Printf("%#v\n", newuser)
+
+		render.JSON(w, http.StatusOK, map[string]interface{}{
+		//"lastid":      num,
+		//"lastidusers": newUser.ID,
+		//"user":  user,
+		//"Error": fmt.Sprintf("%s", err),
+		})
+	}))
 	goji.Serve()
 }
